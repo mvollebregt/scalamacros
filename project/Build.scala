618ecca999cad6d552592691a8793eb9e52fa4a0
mvollebregt@xs4all.nl <mvollebregt@xs4all.nl>
2013-02-20 20:29:16 +0100

    Initial project setup

---
+++ project/Build.scala
@@ -0,0 +1,36 @@
+import sbt._
+import Keys._
+
+object BuildSettings {
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization  := "com.github.mvollebregt",
+    version       := "0.1",
+    scalaVersion  := "2.10.0",
+    scalacOptions ++= Seq("-feature", "-unchecked", "-deprecation")
+  )
+}
+
+object ScalaMacroDebugBuild extends Build {
+  import BuildSettings._
+
+  lazy val root: Project = Project(
+    "root",
+    file("."),
+    settings = buildSettings
+  ) aggregate(macros, examples)
+
+  lazy val macros: Project = Project(
+    "macros",
+    file("macros"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _))
+  )
+
+  lazy val examples: Project = Project(
+    "examples",
+    file("examples"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies += "org.specs2" %% "specs2" % "1.13" % "test"
+    )
+  ) dependsOn(macros)
+}
